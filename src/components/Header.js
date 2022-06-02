2448ea5e434caeed41866e435cecc3541dbe4568
Rinku Saini <rinku.saini@octalinfosolution.com>
2022-06-02 14:59:21 +0530

    changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Header = () => {
-  return <div>This is header part</div>;
+  return <div>This is header part
+
+  <h1>dsaiooaid</h1>
+
+  </div>;
 };
 
 export default Header;
